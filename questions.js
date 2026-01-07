60e06b64113d467267b89d47f89448b6d66e6b97
manabumantappu <azishachigo@gmail.com>
2026-01-07 22:27:34 +0700

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,16 @@
+[
+  {
+    "id": 1,
+    "image": "assets/product1.jpg",
+    "correctArea": "top-left",
+    "correctNG": "キズ",
+    "reason": "表面にキズあり"
+  },
+  {
+    "id": 2,
+    "image": "assets/product2.jpg",
+    "correctArea": "bottom-right",
+    "correctNG": "汚れ",
+    "reason": "異物・汚れ付着"
+  }
+]
